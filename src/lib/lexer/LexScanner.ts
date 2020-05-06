039bffdfde10fce25b29221e78c6ab85bb94c61b
some_guy_oninternet <nonamereal@libero.it>
2020-05-06 21:05:39 +0200

    Added multi-line comments

--- src/lib/lexer/LexScanner.ts
+++ src/lib/lexer/LexScanner.ts
@@ -9,15 +9,15 @@ import { Keywords } from './Keywords';
 let hadError: boolean = false;
 
 class LexScanner {
-	private tokens: Token[] = [];
-	private start: number = 0;
-	private current: number = 0;
-	private line: number = 0;
+	public tokens: Token[] = [];
+	public start: number = 0;
+	public current: number = 0;
+	public line: number = 0;
 
-	constructor(private source: string, private fileName: string) {}
+	constructor(public source: string, public fileName: string) {}
 
 	scan(): Token[] {
-		while (this.current >= this.source.length) {
+		while (this.current < this.source.length) {
 			// we get the next lexeme
 			this.start = this.current;
 			this.scanToken();
@@ -26,72 +26,116 @@ class LexScanner {
 		return this.tokens;
 	}
 
-	private nextChar(): string {
-		this.current++;
+	nextChar(skip: number = 1): string {
+		this.current += skip;
 		return this.source.charAt(this.current - 1);
 	}
 
-	private scanToken() {
+	scanToken() {
 		const char: string = this.nextChar();
-		const addToken: Function = this.addToken;
 
 		switch (char) {
 			case '{':
-				addToken(TokenType.LEFT_CURLY);
+				this.addToken(TokenType.LEFT_CURLY);
 				break;
 			case '}':
-				addToken(TokenType.RIGHT_CURLY);
+				this.addToken(TokenType.RIGHT_CURLY);
 				break;
 			case '[':
-				addToken(TokenType.LEFT_BRACKET);
+				this.addToken(TokenType.LEFT_BRACKET);
 				break;
 			case ']':
-				addToken(TokenType.RIGHT_BRACKET);
+				this.addToken(TokenType.RIGHT_BRACKET);
 				break;
 			case '(':
-				addToken(TokenType.LEFT_PAREN);
+				this.addToken(TokenType.LEFT_PAREN);
 				break;
 			case ')':
-				addToken(TokenType.RIGHT_PAREN);
+				this.addToken(TokenType.RIGHT_PAREN);
 				break;
 			case ';':
-				addToken(TokenType.SEMI_COL);
+				this.addToken(TokenType.SEMI_COL);
 				break;
 			case ':':
-				addToken(TokenType.COLON);
+				this.addToken(TokenType.COLON);
 				break;
 			case ',':
-				addToken(TokenType.COMMA);
+				this.addToken(TokenType.COMMA);
 				break;
 			case '*':
-				addToken(this.match('*') ? TokenType.POW : this.match('=') ? TokenType.TIMES_EQ : TokenType.MULTIPLY);
+				this.addToken(
+					this.match('*')
+						? TokenType.POW
+						: this.match('=')
+							? TokenType.TIMES_EQ
+							: this.match('/')
+								? throws(new SyntaxError('Unexpected multiline comment ending.'), this.fileName, {
+										line: this.line,
+										column: this.current,
+										code: 'TO_BE_REPLACED',
+										exit: true
+									})
+								: TokenType.MULTIPLY
+				);
 				break;
 			case '!':
-				addToken(this.match('=') ? TokenType.NOT_STRICT_EQ : TokenType.NOT);
+				this.addToken(this.match('=') ? TokenType.NOT_STRICT_EQ : TokenType.NOT);
 				break;
 			case '=':
-				addToken(this.match('=') ? TokenType.STRICT_EQ : this.match('>') ? TokenType.FAT_ARROW : TokenType.EQ);
+				this.addToken(
+					this.match('=') ? TokenType.STRICT_EQ : this.match('>') ? TokenType.FAT_ARROW : TokenType.EQ
+				);
 				break;
 			case '+':
-				addToken(this.match('=') ? TokenType.PLUS_EQ : TokenType.PLUS);
+				this.addToken(this.match('=') ? TokenType.PLUS_EQ : TokenType.PLUS);
 				break;
 			case '-':
-				addToken(this.match('=') ? TokenType.MINUS_EQ : this.match('>') ? TokenType.ARROW : TokenType.MINUS);
+				this.addToken(
+					this.match('=') ? TokenType.MINUS_EQ : this.match('>') ? TokenType.ARROW : TokenType.MINUS
+				);
 				break;
 			case '<':
-				addToken(this.match('=') ? TokenType.LESS_OR_EQ_THAN : TokenType.LESS_THAN);
+				this.addToken(this.match('=') ? TokenType.LESS_OR_EQ_THAN : TokenType.LESS_THAN);
 				break;
 			case '>':
-				addToken(this.match('=') ? TokenType.GREATER_OR_EQ_THAN : TokenType.GREATER_THAN);
+				this.addToken(this.match('=') ? TokenType.GREATER_OR_EQ_THAN : TokenType.GREATER_THAN);
 				break;
 			case '&':
-				this.match('&') ? addToken(TokenType.AND) : '';
+				this.match('&') ? this.addToken(TokenType.AND) : '';
 				break;
 			case '/':
 				if (this.match('/')) {
 					while (this.peek() !== '\n' && !(this.current >= this.source.length)) this.nextChar();
+				} else if (this.match('*')) {
+					let multilines: number = 0;
+					multilines++;
+
+					while (multilines !== 0 && !(this.current >= this.source.length)) {
+						this.nextChar();
+						if (this.peek() === '/' && this.peek(1) === '*') {
+							multilines++;
+						}
+
+						if (this.peek() === '*' && this.peek(1) === '/') {
+							multilines--;
+						}
+					}
+					// multilines isn't zero so we throw an error
+					if (multilines) {
+						throws(
+							new SyntaxError('Expected multiline comment end, but found end of file.'),
+							this.fileName,
+							{
+								line: this.line,
+								column: this.current,
+								code: 'TO_BE_REPLACED',
+								exit: true
+							}
+						);
+					}
+					this.nextChar(2);
 				} else {
-					addToken(this.match('=') ? TokenType.DIV_EQ : TokenType.DIV);
+					this.addToken(this.match('=') ? TokenType.DIV_EQ : TokenType.DIV);
 				}
 				break;
 			case ' ':
@@ -112,6 +156,7 @@ class LexScanner {
 				} else if (this.isAlpha(char)) {
 					this.konaIdentifier();
 				} else {
+					console.log(char);
 					throws(new SyntaxError("Unexpected character: '" + char), this.fileName, {
 						line: this.line,
 						column: this.current,
@@ -123,12 +168,15 @@ class LexScanner {
 		}
 	}
 
-	private addToken(type: TokenType, literal?: any) {
+	addToken(type: TokenType | void, literal?: any) {
 		const text: string = this.source.substring(this.start, this.current);
+		if (typeof type === 'undefined') {
+			return;
+		}
 		this.tokens.push(new Token(type, text, literal === undefined ? null : literal, this.line));
 	}
 
-	private match(expected: string): boolean {
+	match(expected: string): boolean {
 		if (this.current >= this.source.length) {
 			return false;
 		}
@@ -142,22 +190,22 @@ class LexScanner {
 
 	// ----------HELPERS---------- //
 
-	private peek(charsToPeek: number = 0): string {
+	peek(charsToPeek: number = 0): string {
 		if (this.current + charsToPeek >= this.source.length) {
 			return '\0';
 		}
 		return this.source.charAt(this.current + charsToPeek);
 	}
 
-	private isEnd(): boolean {
+	isEnd(): boolean {
 		return this.current >= this.source.length;
 	}
 
-	private isAlpha(char: string): boolean {
+	isAlpha(char: string): boolean {
 		return /[a-zA-Z_]/g.test(char);
 	}
 
-	private isAlphaNum(char: string): boolean {
+	isAlphaNum(char: string): boolean {
 		return this.isAlpha(char) || this.isDigit(char);
 	}
 
@@ -210,3 +258,5 @@ class LexScanner {
 
 	// -------DEFINITIONS------ //
 }
+
+export { LexScanner };
