4e1a6b44e5e130d131a325aee5b6e42fed67fe85
some_guy_oninternet <nonamereal@libero.it>
2020-05-07 19:52:49 +0200

    Make lexeme public

--- src/lib/lexer/Token.ts
+++ src/lib/lexer/Token.ts
@@ -1,12 +1,7 @@
 import { TokenType } from './TokenTypes';
 
 class Token {
-	constructor(
-		private type: TokenType,
-		private lexeme: string,
-		private literal: string | null,
-		private line: number
-	) {}
+	constructor(private type: TokenType, public lexeme: string, private literal: string | null, private line: number) {}
 
 	toString() {
 		return this.type + ' ' + this.lexeme + ' ' + this.literal;
