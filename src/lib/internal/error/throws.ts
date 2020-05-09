6a9e33bdf4c08748fc8a423662537947e5ed7f0b
some_guy_oninternet <nonamereal@libero.it>
2020-05-09 16:14:22 +0200

    Change colors and add functioning chalk import

--- src/lib/internal/error/throws.ts
+++ src/lib/internal/error/throws.ts
@@ -1,14 +1,21 @@
-import { KonaError } from './InternalKonaError';
-import * as chalk from 'chalk';
+import { KonaError } from './errorTypes/InternalKonaError';
+import * as chalkImport from 'chalk';
+
+// TypeScript translates the chalk import to __importStar(require('chalk'))
+// and not to require('chalk'), that returns a module object with a property
+// named default
+// so chalk methods aren't accessible without doing this
+
+const chalk = chalkImport.default;
 
 function throws(
 	konaerror: KonaError,
 	filename: string,
 	info: { line: number; column: number; code: string; exit?: true | false }
 ) {
-	console.log(chalk.redBright(konaerror.errorType) + ': ' + chalk.whiteBright(konaerror.message));
-	console.log('    ' + info.code.trim());
-	console.log(chalk.bold.cyanBright('\n    at: ' + filename + ' ' + info.line + ':' + info.column));
+	console.log(chalk.bold.redBright(konaerror.errorType) + ': ' + chalk.whiteBright(konaerror.message));
+	console.log('    ' + chalk.bold.whiteBright(info.code.trim()));
+	console.log(chalk.bold.cyanBright('\n    at: ' + filename + ' ' + info.line + ':' + info.column + '\n'));
 	if (info.exit) {
 		process.exit(1);
 	}
