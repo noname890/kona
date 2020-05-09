8c20c7f523a552bb02aba1d7287bb47d229ac826
some_guy_oninternet <nonamereal@libero.it>
2020-05-09 16:13:07 +0200

    Make properties public

--- src/lib/lexer/Token.ts
+++ src/lib/lexer/Token.ts
@@ -1,7 +1,7 @@
 import { TokenType } from './TokenTypes';
 
 class Token {
-	constructor(private type: TokenType, public lexeme: string, private literal: string | null, private line: number) {}
+	constructor(public type: TokenType, public lexeme: string, public literal: string | null, private line: number) {}
 
 	toString() {
 		return this.type + ' ' + this.lexeme + ' ' + this.literal;
