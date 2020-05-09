6f953d1e535af2ff12f1e354167fd4e5292826ef
some_guy_oninternet <nonamereal@libero.it>
2020-05-09 19:58:06 +0200

    Added column property

--- src/lib/lexer/Token.ts
+++ src/lib/lexer/Token.ts
@@ -1,7 +1,13 @@
 import { TokenType } from './TokenTypes';
 
 class Token {
-	constructor(public type: TokenType, public lexeme: string, public literal: string | null, private line: number) {}
+	constructor(
+		public type: TokenType,
+		public lexeme: string,
+		public literal: string | null,
+		public line: number,
+		public column?: number
+	) {}
 
 	toString() {
 		return this.type + ' ' + this.lexeme + ' ' + this.literal;
