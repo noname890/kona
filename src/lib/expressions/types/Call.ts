aeda92a90b2476521ecda6e7a3b6438993a75d0f
some_guy_oninternet <nonamereal@libero.it>
2020-05-09 16:12:36 +0200

    Add Call.ts file

---
+++ src/lib/expressions/types/Call.ts
@@ -0,0 +1,15 @@
+import { Expression } from '../Expression';
+import { Token } from '../../lexer/Token';
+import { ExpVisitors } from '../ExpVisitors';
+
+class Call extends Expression {
+	constructor(public callee: Expression, public parenthesis: Token, public args: Expression[]) {
+		super();
+	}
+
+	public accept(visitor: ExpVisitors): void {
+		return visitor.visitCall(this);
+	}
+}
+
+export { Call };
