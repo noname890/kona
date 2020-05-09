bae06b890a2ca30efc51042084b70a33dc6c81c6
some_guy_oninternet <nonamereal@libero.it>
2020-05-09 16:13:22 +0200

    Add visitCall

--- src/lib/expressions/ExpVisitors.ts
+++ src/lib/expressions/ExpVisitors.ts
@@ -5,12 +5,14 @@ import { Binary } from './types/Binary';
 import { Unary } from './types/Unary';
 import { Literal } from './types/Literal';
 import { Group } from './types/Group';
+import { Call } from './types/Call';
 
 interface ExpVisitors {
 	visitBinary(visitor: Binary): any;
 	visitUnary(unary: Unary): any;
 	visitLiteral(literal: Literal): any;
 	visitGrouping(group: Group): any;
+	visitCall(call: Call): any;
 }
 
 export { ExpVisitors };
