04dbd5ac7a2d4e3a0494869860e6079925fbb78d
some_guy_oninternet <nonamereal@libero.it>
2020-05-13 20:55:00 +0200

    Move ReferenceError

---
+++ src/lib/internal/error/errorTypes/runtime/ReferenceError.ts
@@ -0,0 +1,11 @@
+import { KonaError } from '../InternalKonaError';
+
+class ReferenceError extends KonaError {
+	public errorType = 'ReferenceError';
+
+	constructor(msg: string) {
+		super(msg);
+	}
+}
+
+export { ReferenceError };
