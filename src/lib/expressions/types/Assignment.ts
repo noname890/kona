35033902a989f61a715bdc52fdb4e2842a21611c
some_guy_oninternet <nonamereal@libero.it>
2020-05-13 20:28:23 +0200

    Add assignment expression

---
+++ src/lib/expressions/types/Assignment.ts
@@ -0,0 +1,15 @@
+import { Expression } from '../Expression';
+import { Token } from '../../lexer/Token';
+import { ExpVisitors } from '../ExpVisitors';
+
+class Assignment extends Expression {
+	constructor(public name: Token, public value: Expression) {
+		super();
+	}
+
+	public accept(visitor: ExpVisitors): any {
+		return visitor.visitAssignment(this);
+	}
+}
+
+export { Assignment };
