a265c4578d2c08acc63bf8dd968095b1fa8de106
some_guy_oninternet <nonamereal@libero.it>
2020-07-18 19:45:28 +0200

    add visitPragmaStmt()

--- src/lib/statements/StmtVisitors.ts
+++ src/lib/statements/StmtVisitors.ts
@@ -2,12 +2,18 @@ import { ExpressionStmt } from './types/ExpressionStmt';
 import { PrintStmt } from './types/PrintStmt';
 import { VariableStmt } from './types/VariableStmt';
 import { BlockStmt } from './types/BlockStmt';
+import { IfStmt } from './types/IfStmt';
+import { WhileStmt } from './types/WhileStmt';
+import { PragmaStatement } from './types/PragmaStmt';
 
 interface StmtVisitors {
 	visitExprStmt(visitor: ExpressionStmt): any;
 	visitPrintStmt(visitor: PrintStmt): any;
 	visitVariableStmt(visitor: VariableStmt): any;
 	visitBlockStmt(visitor: BlockStmt): any;
+	visitIfStmt(visitor: IfStmt): any;
+	visitWhileStmt(visitor: WhileStmt): any;
+	visitPragmaStmt(visitBlockStmt: PragmaStatement): any;
 }
 /* eslint no-undef: off */
 export { StmtVisitors };
