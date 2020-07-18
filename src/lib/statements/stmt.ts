c61c58614528b5dc863656d97faccff9a13ef198
some_guy_oninternet <nonamereal@libero.it>
2020-07-18 19:43:17 +0200

    export Pragma

--- src/lib/statements/stmt.ts
+++ src/lib/statements/stmt.ts
@@ -2,3 +2,6 @@ export * from './types/ExpressionStmt';
 export * from './types/PrintStmt';
 export * from './types/VariableStmt';
 export * from './types/BlockStmt';
+export * from './types/IfStmt';
+export * from './types/WhileStmt';
+export * from './types/PragmaStmt';
