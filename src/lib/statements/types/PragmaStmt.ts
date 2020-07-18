9f3a3f7bd3636db3140bb20409eb7e331e92f701
some_guy_oninternet <nonamereal@libero.it>
2020-07-18 19:41:48 +0200

    add pragmaStmt

---
+++ src/lib/statements/types/PragmaStmt.ts
@@ -0,0 +1,15 @@
+import { Statement } from '../Statements';
+import { StmtVisitors } from '../StmtVisitors';
+import { Token } from '../../lexer/Token';
+
+class PragmaStatement extends Statement {
+	constructor(public pragmaTarget: Token, public pragmaArg: Token) {
+		super();
+	}
+
+	public accept(visitor: StmtVisitors): void {
+		return visitor.visitPragmaStmt(this);
+	}
+}
+
+export { PragmaStatement };
