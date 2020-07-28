43e694268f86a93cbc1eef2ac29a3b7f1c13e99f
some_guy_oninternet <nonamereal@libero.it>
2020-07-28 17:17:21 +0200

    add shebang at the top

--- src/bin/kona.ts
+++ src/bin/kona.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 import { Interpreter } from '../lib/interpreter/interpreter';
 import { LexScanner } from '../lib/lexer/LexScanner';
 import { Parser } from '../lib/parser/Parser';
