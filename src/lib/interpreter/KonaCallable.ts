4071b78e9cec638c099d844837e8743a4f8f12ef
some_guy_oninternet <nonamereal@libero.it>
2020-08-22 19:28:17 +0200

    add JSDoc comment

--- src/lib/interpreter/KonaCallable.ts
+++ src/lib/interpreter/KonaCallable.ts
@@ -1,6 +1,9 @@
 import { Interpreter } from './interpreter';
 import { Token } from '../lexer/Token';
 
+/**
+ * Interface that describes a function
+ */
 export default interface KonaCallable {
     arity(): number;
     callFn(interpreter: Interpreter, fnArguments: any[], fnToken: Token): any;
