7cdd7f56b5e7e8a52e9d996db13ea270e061d982
some_guy_oninternet <nonamereal@libero.it>
2020-08-22 19:36:44 +0200

    add JSDoc comments

--- src/lib/interpreter/Environment.ts
+++ src/lib/interpreter/Environment.ts
@@ -3,7 +3,10 @@ import { throws } from '../internal/error/throws';
 import { ReferenceError } from '../internal/error/errorTypes/runtime/ReferenceError';
 import Stack from './Stack';
 
-class Environment {
+/**
+ * Environment that holds vars, consts, pragmas and handles errors
+ */
+export default class Environment {
 	private vars: any = {};
 	private pragmas: any = {};
 	private constants: string[] = [];
@@ -13,6 +16,10 @@ class Environment {
 
 	constructor(private fileName: string, public enclosing: Environment | null, private stack: Stack) {}
 
+	/**
+	 * Returns a pragma
+	 * @param name name of the pragma
+	 */
 	public getPragma(name: string): any {
 		if (this.pragmas.hasOwnProperty(name)) {
 			return this.pragmas[name];
@@ -22,6 +29,10 @@ class Environment {
 		}
 	}
 
+	/**
+	 * Returns a variable, throws if it does not exist
+	 * @param name name of the var
+	 */
 	public getVar(name: Token): any {
 		if (this.vars.hasOwnProperty(name.lexeme)) {
 			return this.vars[name.lexeme];
@@ -31,6 +42,7 @@ class Environment {
 			return this.enclosing.getVar(name);
 		}
 
+		// vars named `_` are not bound, unless specified with a pragma
 		if (name.lexeme === '_' && !this.getPragma('allow_underscore_for_var_names')) {
 			throws(
 				new ReferenceError(`Undefined variable '_'.\nVariables that are named '_' are not assigned.`),
@@ -56,6 +68,13 @@ class Environment {
 		});
 	}
 
+	/**
+	 * Defines a constant, if a variable exists under the same name
+	 * converts it into a constant, const casts are only valid in
+	 * the scope they were casted in
+	 * @param name name of the constant
+	 * @param value value of the constant
+	 */
 	public defineConst(name: string, value: any): void {
 		if (name === '_' && !this.getPragma('allow_underscore_for_var_names')) {
 			return;
@@ -67,6 +86,13 @@ class Environment {
 		this.constants.push(name);
 	}
 
+	/**
+	 * Creates a variable and assigns a value.
+	 * Only binds variables that are not named `_`, unless specified with
+	 * a pragma
+	 * @param name name of the var
+	 * @param value value of the var
+	 */
 	public define(name: string, value: any): void {
 		if (name === '_' && !this.getPragma('allow_underscore_for_var_names')) {
 			return;
@@ -77,10 +103,20 @@ class Environment {
 		this.vars[name] = value;
 	}
 
+	/**
+	 * Defines a pragma
+	 * @param name name of the pragma
+	 * @param value value if the pragma
+	 */
 	public definePragma(name: string, value: string): void {
 		this.pragmas[name] = value;
 	}
 
+	/**
+	 * Assigns to a variable, throws if it is a constant, isn't a variable, or it does not exist
+	 * @param name name of the variable
+	 * @param value value to assign
+	 */
 	public assign(name: Token, value: any): null {
 		if (this.isConst(name.lexeme)) {
 			throws(new ReferenceError('Illegal assignment to constant variable.'), this.fileName, {
@@ -130,12 +166,21 @@ class Environment {
 		return null;
 	}
 
+	/**
+	 * Checks if a variable exists an any scope
+	 * @param name name of the variable
+	 */
 	private exists(name: string): boolean {
 		if (this.vars.hasOwnProperty(name)) return true;
 		if (this.enclosing) return this.enclosing.exists(name);
 		return false;
 	}
 
+	/**
+	 * Checks if a variable is a const cast.
+	 * Used by `assign` to give hints
+	 * @param name name of the variable
+	 */
 	private isCast(name: string): boolean {
 		if (this.casts.includes(name)) {
 			return true;
@@ -145,6 +190,10 @@ class Environment {
 		return false;
 	}
 
+	/**
+	 * Checks if the variable specified is a constant
+	 * @param name name of the const
+	 */
 	private isConst(name: string): boolean {
 		if (this.constants.includes(name)) {
 			return true;
@@ -154,5 +203,3 @@ class Environment {
 		return false;
 	}
 }
-
-export { Environment };
