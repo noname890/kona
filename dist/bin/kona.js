687e995925dd0a4c38cb09539c9b8c992aa71396
some_guy_oninternet <nonamereal@libero.it>
2020-08-22 20:07:27 +0200

    build

--- dist/bin/kona.js
+++ dist/bin/kona.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var interpreter_1 = require("../lib/interpreter/interpreter");
+var interpreter_1 = __importDefault(require("../lib/interpreter/interpreter"));
 var LexScanner_1 = require("../lib/lexer/LexScanner");
 var Parser_1 = require("../lib/parser/Parser");
 var fs_1 = require("fs");
@@ -20,7 +23,7 @@ function run(source, fileName) {
     // lex the file contents
     var lexed = new LexScanner_1.LexScanner(source, fileName).scan();
     var parsed = new Parser_1.Parser(lexed, fileName).parse();
-    var interpreter = new interpreter_1.Interpreter(fileName);
+    var interpreter = new interpreter_1.default(fileName);
     if (parsed) {
         interpreter.interpret(parsed);
     }
